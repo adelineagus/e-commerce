eca275f558aa6f8df9cee1ec886cb720a30c1b9d
Adeline <adelineaguspranoto@gmail.com>
2022-07-30 17:48:21 -0700

    fix minor get bugs

--- 02-Challenge/Develop/routes/api/product-routes.js
+++ 02-Challenge/Develop/routes/api/product-routes.js
@@ -22,8 +22,8 @@ router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try{
-    const productData= await Product.findbyPk(req.params.id, {
-      include: [{model:Category}, {model:Tag}]
+    const productData= await Product.findByPk(req.params.id, {
+    include: [{model:Category}, {model:Tag}]
     });
     if(!productData){
       res.status(404).json({message: 'No product found with this id!'})
